a16debb78005e7c56310f00467416b1862fb9a8f
Stefano J. Attardi <stefano@attardi.org>
2021-03-28 16:11:31 -0700

    Add checking of hook dependencies

--- __tests__/react-usememo-required.ts
+++ __tests__/react-usememo-required.ts
@@ -47,21 +47,21 @@ ruleTester.run("useMemo", rule, {
         const myObject = {};
         return <Child prop={myObject} />;
       }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "object-usememo-props" }],
     },
     {
       code: `const Component = () => {
         const myArray = [];
         return <Child prop={myArray} />;
       }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "array-usememo-props" }],
     },
     {
       code: `const Component = () => {
         const myInstance = new Object();
         return <Child prop={myInstance} />;
       }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "instance-usememo-props" }],
     },
     {
       code: `const Component = () => {
@@ -69,33 +69,135 @@ ruleTester.run("useMemo", rule, {
         myObject = {a: 'b'};
         return <Child prop={myObject} />;
       }`,
-      errors: [{ messageId: "usememo-const-required" }],
+      errors: [{ messageId: "usememo-const" }],
     },
     {
       code: `const Component = () => {
         return <Child prop={{}} />;
       }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "object-usememo-props" }],
     },
     {
       code: `const Component = () => {
         return <Child prop={[]} />;
       }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "array-usememo-props" }],
     },
     {
       code: `const Component = () => {
       const myObject = memoize({});
       return <Child prop={myObject} />;
     }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "object-usememo-props" }],
     },
     {
       code: `const Component = () => {
       const myArray = lodash.memoize([]);
       return <Child prop={myArray} />;
     }`,
-      errors: [{ messageId: "usememo-required" }],
+      errors: [{ messageId: "array-usememo-props" }],
+    },
+    {
+      code: `const Component = () => {
+      const myArray1 = [];
+      const myArray2 = React.useMemo(() => myArray1, [myArray1]);
+      return <Child prop={myArray2} />;
+    }`,
+      errors: [{ messageId: "array-usememo-deps" }],
+    },
+  ],
+});
+
+ruleTester.run("useCallback", rule, {
+  valid: [
+    {
+      code: `const Component = () => {
+      const myFn = React.useCallback(function() {}, []);
+      return <Child prop={myFn} />;
+    }`,
+    },
+    {
+      code: `const Component = () => {
+      const myFn = useCallback(() => {}, []);
+      return <Child prop={myFn} />;
+    }`,
+    },
+    {
+      code: `const Component = () => {
+        const myFn = function() {};
+        return <div prop={myFn} />;
+      }`,
+    },
+    {
+      code: `const Component = () => {
+        const myFn = () => {};
+        return <div prop={myFn} />;
+      }`,
+    },
+    {
+      code: `const Component = () => {
+      const myFn1 = useCallback(() => [], []);
+      const myFn2 = React.useCallback(() => myFn1, [myFn1]);
+      return <Child prop={myFn2} />;
+      }`,
+    },
+  ],
+  invalid: [
+    {
+      code: `const Component = () => {
+        const myFn = function myFn() {};
+        return <Child prop={myFn} />;
+      }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+        const myFn = () => {};
+        return <Child prop={myFn} />;
+      }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+        let myFn = useCallback(() => ({}));
+        myFn = () => ({});
+        return <Child prop={myFn} />;
+      }`,
+      errors: [{ messageId: "usecallback-const" }],
+    },
+    {
+      code: `const Component = () => {
+        return <Child prop={() => {}} />;
+      }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+        return <Child prop={() => []} />;
+      }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+      const myFn = memoize(() => {});
+      return <Child prop={myFn} />;
+    }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+      const myFn = lodash.memoize(() => []);
+      return <Child prop={myFn} />;
+    }`,
+      errors: [{ messageId: "function-usecallback-props" }],
+    },
+    {
+      code: `const Component = () => {
+      const myFn1 = () => [];
+      const myFn2 = React.useCallback(() => myFn1, [myFn1]);
+      return <Child prop={myFn2} />;
+      }`,
+      errors: [{ messageId: "function-usecallback-deps" }],
     },
   ],
 });
--- react-usememo-required.ts
+++ react-usememo-required.ts
@@ -3,6 +3,7 @@ import * as ESTree from "estree";
 import { TSESTree } from "@typescript-eslint/types";
 
 const componentNameRegex = /^[^a-z]/;
+const hookNameRegex = /^use[A-Z0-9].*$/;
 
 function isComplexComponent(node: TSESTree.JSXOpeningElement) {
   if (node.type !== "JSXOpeningElement") return false;
@@ -10,7 +11,22 @@ function isComplexComponent(node: TSESTree.JSXOpeningElement) {
   return componentNameRegex.test(node.name.name);
 }
 
-function isUseMemoCallExpression(node: Rule.Node) {
+function isHook(node: TSESTree.Node) {
+  if (node.type === "Identifier") {
+    return hookNameRegex.test(node.name);
+  } else if (
+    node.type === "MemberExpression" &&
+    !node.computed &&
+    isHook(node.property)
+  ) {
+    const obj = node.object;
+    return obj.type === "Identifier" && obj.name === "React";
+  } else {
+    return false;
+  }
+}
+
+function isCallExpression(node: Rule.Node, name: "useMemo" | "useCallback") {
   if (node.type !== "CallExpression") return false;
   if (node.callee.type === "MemberExpression") {
     const {
@@ -20,27 +36,111 @@ function isUseMemoCallExpression(node: Rule.Node) {
       object.type === "Identifier" &&
       property.type === "Identifier" &&
       object.name === "React" &&
-      property.name === "useMemo"
+      property.name === name
     ) {
       return true;
     }
-  } else if (
-    node.callee.type === "Identifier" &&
-    node.callee.name === "useMemo"
-  ) {
+  } else if (node.callee.type === "Identifier" && node.callee.name === name) {
     return true;
   }
 
   return false;
 }
 
+function checkIdentifier(
+  context: Rule.RuleContext,
+  { name }: TSESTree.Identifier,
+  isDeps?: boolean
+) {
+  const variable = context.getScope().variables.find((v) => v.name === name);
+  if (variable === undefined) return;
+  const [{ node }] = variable.defs;
+  if (node.type === "VariableDeclarator") {
+    const { init, parent } = node;
+
+    let currentNode = init;
+    while (currentNode.type === "CallExpression") {
+      if (isCallExpression(currentNode, "useMemo")) {
+        if (parent.kind === "let") {
+          context.report({ node, messageId: "usememo-const" });
+        }
+        return;
+      } else if (isCallExpression(currentNode, "useCallback")) {
+        if (parent.kind === "let") {
+          context.report({ node, messageId: "usecallback-const" });
+        }
+        return;
+      }
+      currentNode = currentNode.arguments[0];
+    }
+
+    checkExpression(context, node, currentNode, isDeps);
+  }
+}
+
+function checkExpression(
+  context: Rule.RuleContext,
+  node: Rule.Node,
+  expression: TSESTree.Expression,
+  isDeps?: boolean
+) {
+  switch (expression.type) {
+    case "ObjectExpression":
+      context.report({
+        node,
+        messageId: isDeps ? "object-usememo-deps" : "object-usememo-props",
+      });
+      break;
+    case "ArrayExpression":
+      context.report({
+        node,
+        messageId: isDeps ? "array-usememo-deps" : "array-usememo-props",
+      });
+      break;
+    case "NewExpression":
+      context.report({
+        node,
+        messageId: isDeps ? "instance-usememo-deps" : "instance-usememo-props",
+      });
+      break;
+    case "FunctionExpression":
+    case "ArrowFunctionExpression":
+      context.report({
+        node,
+        messageId: isDeps
+          ? "function-usecallback-deps"
+          : "function-usecallback-props",
+      });
+      break;
+    case "Identifier":
+      checkIdentifier(context, expression, isDeps);
+      break;
+  }
+}
+
 const rule: Rule.RuleModule = {
   meta: {
     messages: {
-      "usememo-required":
+      "object-usememo-props":
         "Object definition needs to be wrapped in React.useMemo() if used as a prop",
-      "usememo-const-required":
-        "React.useMemo() needs to be assigned to a const to prevent reassignment",
+      "object-usememo-deps":
+        "Object definition needs to be wrapped in React.useMemo() if used as a hook dependency",
+      "array-usememo-props":
+        "Array definition needs to be wrapped in React.useMemo() if used as a prop",
+      "array-usememo-deps":
+        "Array definition needs to be wrapped in React.useMemo() if used as a hook dependency",
+      "instance-usememo-props":
+        "Object instantiation needs to be wrapped in React.useMemo() if used as a hook dependency",
+      "instance-usememo-deps":
+        "Object instantiation needs to be wrapped in React.useMemo() if used as a prop",
+      "function-usecallback-props":
+        "Function definition needs to be wrapped in React.useCallback() if used as a hook dependency",
+      "function-usecallback-deps":
+        "Function definition needs to be wrapped in React.useCallback() if used as a hook dependency",
+      "usememo-const":
+        "React.useMemo() should be assigned to a const to prevent reassignment",
+      "usecallback-const":
+        "React.useCallback() should be assigned to a const to prevent reassignment",
     },
   },
   create: (context) => ({
@@ -51,51 +151,27 @@ const rule: Rule.RuleModule = {
       if (!isComplexComponent(parent)) return;
       if (value.type === "JSXExpressionContainer") {
         const { expression } = value;
-
-        if (
-          expression.type === "ObjectExpression" ||
-          expression.type === "ArrayExpression" ||
-          expression.type === "NewExpression"
-        ) {
-          context.report({ node: node, messageId: "usememo-required" });
-          return;
+        if (expression.type !== "JSXEmptyExpression") {
+          checkExpression(context, node, expression);
         }
+      }
+    },
 
-        if (expression.type === "Identifier") {
-          const { name } = expression;
-          const variable = context
-            .getScope()
-            .variables.find((v) => v.name === name);
-          if (variable === undefined) return;
-          const [{ node }] = variable.defs;
-          if (node.type === "VariableDeclarator") {
-            const { init, parent } = node;
-
-            if (parent.kind === "let") {
-              context.report({
-                node: node,
-                messageId: "usememo-const-required",
-              });
-            }
-
-            let currentNode = init;
-            while (currentNode.type === "CallExpression") {
-              if (isUseMemoCallExpression(currentNode)) {
-                return;
-              }
-              currentNode = currentNode.arguments[0];
-            }
-
-            if (
-              currentNode.type === "ObjectExpression" ||
-              currentNode.type === "ArrayExpression" ||
-              currentNode.type === "NewExpression"
-            ) {
-              context.report({
-                node: currentNode,
-                messageId: "usememo-required",
-              });
-            }
+    CallExpression: (node) => {
+      const { callee } = (node as unknown) as TSESTree.CallExpression &
+        Rule.NodeParentExtension;
+      if (!isHook(callee)) return;
+      const {
+        arguments: [, dependencies],
+      } = (node as unknown) as TSESTree.CallExpression &
+        Rule.NodeParentExtension;
+      if (
+        dependencies !== undefined &&
+        dependencies.type === "ArrayExpression"
+      ) {
+        for (const dep of dependencies.elements) {
+          if (dep !== null && dep.type === "Identifier") {
+            checkIdentifier(context, dep, true);
           }
         }
       }
