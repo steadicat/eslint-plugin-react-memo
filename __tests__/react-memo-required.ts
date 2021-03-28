2ae798104eaae9d4316983b467a65d12e2fb1933
Stefano J. Attardi <stefano@attardi.org>
2021-03-28 11:44:54 -0700

    React.memo lint rule

---
+++ __tests__/react-memo-required.ts
@@ -0,0 +1,72 @@
+import { RuleTester } from "eslint";
+import { rules } from "../react-memo-required";
+
+const ruleTester = new RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
+});
+
+ruleTester.run("memo", rules["react-memo-required"], {
+  valid: [
+    {
+      code: `const Component = React.memo(() => <div />)`,
+    },
+    {
+      code: `const Component = memo(() => <div />)`,
+    },
+    {
+      code: `const Component = memo(useRef(() => <div />))`,
+    },
+    {
+      code: `const Component = React.useRef(React.memo(() => <div />))`,
+    },
+    {
+      code: `const myFunction = () => <div />`,
+    },
+    {
+      code: `const myFunction = wrapper(() => <div />)`,
+    },
+    {
+      code: `const Component = React.memo(function() { return <div />; });`,
+    },
+    {
+      code: `const Component = memo(function Component() { return <div />; });`,
+    },
+    {
+      code: `const myFunction = () => <div />`,
+    },
+    {
+      code: `const myFunction = wrapper(() => <div />)`,
+    },
+    {
+      code: `function myFunction() { return <div />; }`,
+    },
+    {
+      code: `const myFunction = wrapper(function () { return <div /> })`,
+    },
+  ],
+  invalid: [
+    {
+      code: `const Component = () => <div />`,
+      errors: [{ messageId: "memo-required" }],
+    },
+    {
+      code: `const Component = useRef(() => <div />)`,
+      errors: [{ messageId: "memo-required" }],
+    },
+    {
+      code: `const Component = function Component() { return <div />; }`,
+      errors: [{ messageId: "memo-required" }],
+    },
+    {
+      code: `const Component = useRef(function() { return <div />; })`,
+      errors: [{ messageId: "memo-required" }],
+    },
+    {
+      code: `function Component() { return <div />; }`,
+      errors: [{ messageId: "memo-required" }],
+    },
+  ],
+});
---
+++ react-memo-required.ts
@@ -0,0 +1,85 @@
+import { Rule } from "eslint";
+import * as ESTree from "estree";
+
+const componentNameRegex = /^[A-Z]/;
+
+function isMemoCallExpression(node: Rule.Node) {
+  if (node.type === "CallExpression") {
+    if (node.callee.type === "MemberExpression") {
+      const {
+        callee: { object, property },
+      } = node;
+      if (
+        object.type === "Identifier" &&
+        property.type === "Identifier" &&
+        object.name === "React" &&
+        property.name === "memo"
+      ) {
+        return true;
+      }
+    } else if (
+      node.callee.type === "Identifier" &&
+      node.callee.name === "memo"
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+function checkFunction(
+  context: Rule.RuleContext,
+  node: (
+    | ESTree.ArrowFunctionExpression
+    | ESTree.FunctionExpression
+    | ESTree.FunctionDeclaration
+  ) &
+    Rule.NodeParentExtension
+) {
+  let currentNode = node.parent;
+  while (currentNode.type === "CallExpression") {
+    if (isMemoCallExpression(currentNode)) {
+      return;
+    }
+
+    currentNode = currentNode.parent;
+  }
+
+  if (currentNode.type === "VariableDeclarator") {
+    const { id } = currentNode;
+    if (id.type === "Identifier") {
+      if (componentNameRegex.test(id.name)) {
+        context.report({ node: currentNode, messageId: "memo-required" });
+      }
+    }
+  } else if (
+    node.type === "FunctionDeclaration" &&
+    currentNode.type === "Program"
+  ) {
+    if (componentNameRegex.test(node.id.name)) {
+      context.report({ node: currentNode, messageId: "memo-required" });
+    }
+  }
+}
+
+export const rules: { [Key: string]: Rule.RuleModule } = {
+  "react-memo-required": {
+    meta: {
+      messages: {
+        "memo-required": "Component definition not wrapped in React.memo()",
+      },
+    },
+    create: (context) => ({
+      ArrowFunctionExpression(node) {
+        checkFunction(context, node);
+      },
+      FunctionDeclaration(node) {
+        checkFunction(context, node);
+      },
+      FunctionExpression(node) {
+        checkFunction(context, node);
+      },
+    }),
+  },
+};
